b7be4da46c363272a43c42b1d7268e74aee71f6c
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-07-19 03:19:23 +0500

    Animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import useWebAnimations from "@wellyshen/use-web-animations";
+
 function App() {
+
+  const { ref, playState, getAnimation } = useWebAnimations({
+    keyframes: [
+      {transform: "translate(0,0)"},
+      {transform: "translate(600px, 0)"}
+    ],
+    timing: {
+     // delay: 2000, // Start with a 500ms delay
+      duration: 1000, // Run for 1000ms
+      iterations: Infinity, // Repeat once
+      direction: "alternate", // Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+    },
+    // onReady: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+    // },
+    // onUpdate: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation enters the running state or changes state
+    // },
+    // onFinish: ({ playState, animate, animation }) => {
+    //   // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+    // },
+    // More useful options...
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+     <div className="target" ref={ref}>          
+        Hello World   
     </div>
+    
+     Current animation state: {playState}
+    <button onClick={() => getAnimation().play()}> Play </button>
+    <button onClick={() => getAnimation().pause()}> Pause </button>
+   </div> 
+    
+    
   );
 }
 
