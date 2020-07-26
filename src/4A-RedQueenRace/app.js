825b3507ce2e71542e912abf09807b3ecbf385e7
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-07-27 01:31:17 +0500

    style.css

---
+++ src/4A-RedQueenRace/app.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+import './style.css'
+
+import useWebAnimations from "@wellyshen/use-web-animations";
+
+export const RedQueenRace  = () => {
+    return (
+        <div>
+            
+        </div>        
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-import useWebAnimations from "@wellyshen/use-web-animations";
+import { RedQueenRace } from './4A-RedQueenRace/app'
 
 function App() {
-
-  const { ref, playState, getAnimation } = useWebAnimations({
-    keyframes: [
-      {transform: "translate(0,0)"},
-      {transform: "translate(600px, 0)"},      
-    ],
-    timing: {
-     // delay: 2000, // Start with a 500ms delay
-      duration: 1000, // Run for 1000ms
-      iterations: Infinity, // Repeat once
-      direction: "alternate", // Run the animation forwards and then backwards
-      easing: "ease-in-out", // Use a fancy timing function
-    },
-    // onReady: ({ playState, animate, animation }) => {
-    //   // Triggered when the animation is ready to play (Google Chrome: available in v84+)
-    // },
-    // onUpdate: ({ playState, animate, animation }) => {
-    //   // Triggered when the animation enters the running state or changes state
-    // },
-    // onFinish: ({ playState, animate, animation }) => {
-    //   // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
-    // },
-    // More useful options...
-  });
-
-
+  
   return (
    <div>
-     <div className="target" ref={ref}>          
-        Hello World   
-    </div>
+     <RedQueenRace />     
+   </div>
+
     
-     Current animation state: {playState}
-    <button onClick={() => getAnimation().play()}> Play </button>
-    <button onClick={() => getAnimation().pause()}> Pause </button>
-   </div> 
+     
     
     
   );
